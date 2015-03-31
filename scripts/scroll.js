6c96b87d97113066ac8157829f465014f8b72086
McLeanBH <mcleanbh@gmail.com>
2015-03-31 16:00:32 -0400

    ScrollMagic Plugin package, and updated dependencies

---
+++ scripts/scroll.js
@@ -0,0 +1,16 @@
+(function(){
+
+  require('ScrollMagic', 'ScrollMagic.debug'), function(ScrollMagic) {
+  // init controller
+  var controller = new ScrollMagic.Controller({
+      globalSceneOptions: {triggerHook: "onCenter"}
+    });
+  // init scene
+  var scene = new ScrollMagic.Scene({
+        duration: 300,
+        offset: 100
+    })
+      .addTo(controller)
+      .addIndicators();
+    };
+})();
